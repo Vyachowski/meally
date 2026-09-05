a0212ff02df8bbd52bfaf0a8107d83c18df8846d
Slava Haikin <slava@Slavas-MacBook-Air.local>
2026-09-05 22:26:45 +0400

    chore: delete all files

--- db/migrate/20260801125634_add_profile_fields_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddProfileFieldsToUsers < ActiveRecord::Migration[8.1]
-  def change
-    add_column :users, :birth_date, :date
-    add_column :users, :sex, :string
-    add_column :users, :height_cm, :integer
-    add_column :users, :activity_factor, :string
-  end
-end
